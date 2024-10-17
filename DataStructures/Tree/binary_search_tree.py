52dc9a08e5102b43d643f723c17f7a8f0c1f4b71
julianramosss04 <jd.ramosg1@uniandes.edu.co>
2024-10-17 11:11:39 -0500

    newnewnewmapmapmap

---
+++ DataStructures/Tree/binary_search_tree.py
@@ -0,0 +1,7 @@
+
+def new_map():
+    """Crea una tabla de símbolos ordenada basada en un árbol binario de búsqueda (BST) vacía."""
+    return {
+        "root": None,
+        "type": "BST"
+    }   
